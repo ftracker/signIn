95986087322afcdbecd5d4d7c22d1f0ce3c4f85e
Daniel Bustamante Ospina <daniel.bustamante@sofka.com.co>
2019-03-11 00:54:57 -0500

    Routes for login and register

--- signup-react/src/App.js
+++ signup-react/src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
 import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 
 class App extends Component {
-  render() {
-    return (
-        <div className="container">
-            <SignIn />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <Route exact path="/" component={SignIn}/>
+                    <Route path="/register" component={SignUp}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
